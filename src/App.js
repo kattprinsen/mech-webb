27028b58795a18e9be427ca4798db3f8a408b280
Kattprinsen <1647312+kattprinsen@users.noreply.github.com>
2021-11-05 10:43:16 +0100

    Responsive Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+
+import Navbar from './components/Navbar/Navbar';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Home from './pages/Home/Home';
+import Shop from './pages/Shop/Shop';
 
 function App() {
   return (
-    <div>
-      <h1> What </h1>
+    <div className="app-container">
+      <Navbar />
+        <Routes>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/shop" component={Shop} />
+        </Routes>
     </div>
   );
 }
