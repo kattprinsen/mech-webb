b61992fab9085494a02473924a3257f57410303c
Kattprinsen <1647312+kattprinsen@users.noreply.github.com>
2021-11-08 10:32:33 +0100

    Improved on css and added content component

---
+++ src/components/Content/Content.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Content.css';
+
+const Content = ({heading, content}) => {
+  heading = heading || 'defaultHeader'; //default prop handler for no heading
+
+  const Heading = ({ heading }) => <h1>{heading}</h1>
+  const ContentText = ({ contentText }) => <p>{contentText}</p>
+
+  return (
+    <div className="content-header">
+      <Heading heading={`Welcome to the ${heading} component`} />
+      <ContentText contentText={content} />
+    </div>
+  );
+}
+
+export default Content;
