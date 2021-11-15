f2b7d2b25bee8d50e7999e78c3a559a68b9974c6
Kattprinsen <1647312+kattprinsen@users.noreply.github.com>
2021-11-15 11:13:20 +0100

    Working on the dropdowns

--- src/components/DropDown/DropDown.js
+++ src/components/DropDown/DropDown.js
@@ -1,29 +1,59 @@
 import React, { useState } from 'react';
 import './DropDown.css';
 
-const options = ["React", "HTML", "JavaScript", "CSS"];
+const countries = ["Sweden", "Norway", "Denmark", "Iceland"];
+const nodes = ["Malmö", "Köpenhamn", "Bergen", "Reykjavik"];
+const stores = ["El-giganten", "Media-markt", "Emporia", "Dustin.se"]
 
 const DropDown = () => {
 
-  const [isOpen, setIsOpen] = useState(false);
+  const [countriesOpen, setCountriesOpen] = useState(false);
+  const [nodesOpen, setNodesOpen] = useState(false);
+  const [storesOpen, setStoresOpen] = useState(false);
+
   const [selectedOption, setSelectedOption] = useState(null);
 
-  const toggling = () => setIsOpen(!isOpen);
+  const toggleCountries = () => setCountriesOpen(!countriesOpen);
+  const toggleNodes = () => setNodesOpen(!nodesOpen);
+  const toggleStores = () => setStoresOpen(!storesOpen);
 
   const onOptionClicked = value => () => {
-    setSelectedOption(value);
-    setIsOpen(false);
-    console.log(selectedOption);
+    countriesOpen(value);
+    setCountriesOpen(false);
   };
 
   return (
     <div className="dropdown-container">
-      <div className="dropdown-header" onClick={toggling}>
-        {selectedOption || "Frontend"}
+      <div className="dropdown-header" onClick={toggleCountries}>
+        {selectedOption || "Countries"}
       </div>
-      {isOpen && (
+      {countriesOpen && (
       <ul className="dropdown-list">
-        {options.map(option => (
+        {countries.map(option => (
+          <li className="dropdown-list-item" onClick={onOptionClicked(option)} key={Math.random()}>
+            {option}
+          </li>
+        ))}
+      </ul>
+      )}
+      <div className="dropdown-header" onClick={toggleNodes}>
+        {selectedOption || "Nodes"}
+      </div>
+      {nodesOpen && (
+      <ul className="dropdown-list">
+        {nodes.map(option => (
+          <li className="dropdown-list-item" onClick={onOptionClicked(option)} key={Math.random()}>
+            {option}
+          </li>
+        ))}
+      </ul>
+      )}
+      <div className="dropdown-header" onClick={toggleStores}>
+        {selectedOption || "Store"}
+      </div>
+      {storesOpen && (
+      <ul className="dropdown-list">
+        {stores.map(option => (
           <li className="dropdown-list-item" onClick={onOptionClicked(option)} key={Math.random()}>
             {option}
           </li>
